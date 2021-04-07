9df3c8b6e57b4a5714773bd144d442a3a4172e1c
Christopher Ramos <25787207+Cramos711@users.noreply.github.com>
2021-04-07 10:09:23 -0400

    added to hero

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,14 @@ import "./components/Triangles";
 
 // GSAP
 import "./gsap/navigation";
+
+// cycle color on Hero section
+(function changeHi() {
+  const hi = document.getElementById("hi");
+  const colors = ["#75c7f4", "#4373d1", "#ff00c7", "#7326ad"];
+  hi.style.transition = "all 1s ease-in-out";
+
+  setInterval(() => {
+    hi.style.color = colors[Math.floor(Math.random() * 4)];
+  }, 1000);
+})();
